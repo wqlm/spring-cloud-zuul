50b4fdf2e97a12a3ddc3d95962e544acfe1384ef
wqlm <649924325@qq.com>
2019-08-16 17:11:12 +0800

    8-2到8-3 zuul 简单使用

---
+++ src/main/java/com/wqlm/zuul/ZuulConfig.java
@@ -0,0 +1,21 @@
+package com.wqlm.zuul;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 远端 git 配置更新之后,动态加载配置文件
+ * @author wqlm
+ * @date 2019/8/16 16:51
+ */
+@Component
+public class ZuulConfig {
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
+    }
+}
